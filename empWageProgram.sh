1e233ac9aec2681b5d49d0e1f5c54afe3c9533ab
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-23 22:09:15 +0530

    calculate EmpWage

--- empWageProgram.sh
+++ empWageProgram.sh
@@ -4,6 +4,18 @@ echo "Welcome Employee Wage Program "
 
 isPresent=1;
 randomCheck=$((RANDOM%2));
+#isPartTime=1;
+#isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+maxHrsInDay=8;
+#numWorkingDays=20;
+
+totalEmpWage=160;
+EmpHrPerDay=8;
+totalWorkingDays=0;
+totalEmpWagePerDay=0;
+
 
 if [ $isPresent -eq $randomCheck ]
 then
@@ -11,3 +23,13 @@ then
 else
         echo "Employee is absent"
 fi
+
+
+if [ $totalEmpWage -gt $toatlEmpWagePerDay ]
+then
+        totalEmpWage=$(($empHrPerDay*$empRatePerHr))
+else
+	echo "in valid details"
+fi
+
+totalSalary=$((totalEmpWage));
