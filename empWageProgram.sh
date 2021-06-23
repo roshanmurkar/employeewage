13cd1fbfca38aaa80d8c354cb02720361bbcfbb4
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-23 22:22:23 +0530

    PartTime

--- empWageProgram.sh
+++ empWageProgram.sh
@@ -5,13 +5,14 @@ echo "Welcome Employee Wage Program "
 
 isPresent=1;
 randomCheck=$((RANDOM%2));
-#isPartTime=1;
-#isFullTime=2;
+isPartTime=1;
+isFullTime=2;
 totalSalary=0;
 empRatePerHr=20;
-maxHrsInDay=8;
-#numWorkingDays=20;
+maxHrsInMonth=10;
+numWorkingDays=20;
 
+totalEmpHrs=0;
 totalEmpWage=160;
 EmpHrPerDay=8;
 totalWorkingDays=0;
@@ -35,3 +36,22 @@ fi
 
 totalSalary=$((totalEmpWage));
 
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numkWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3));
+        case $empCheck in
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
+totalSalary=$(($totalEmpHrs*$empRatePerHr));
